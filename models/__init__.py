a0719abaae9784f93096bab88b61450dfcc44a9d
OndrejV <vachao@fzu.cz>
2026-03-17 14:55:33 +0100

    added itk

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 from .detectors.model import detectors_model
 from .particles.model import particles_model
 from .fram.model import fram_model
-
-__all__ = ["particles_model", "detectors_model", "fram_model"]
+from .atlas_itk.model import atlas_itk_model
+__all__ = ["particles_model", "detectors_model", "fram_model", "atlas_itk_model"]
