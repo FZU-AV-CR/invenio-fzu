402431c2a751c0478ea04f894c1d25d4ed273fe9
Michal <michal.lukes3@gmail.com>
2026-06-03 15:28:55 +0200

    Improved sampling of testing Delphi metadata and a new facet for number of events

--- models/particles/model.py
+++ models/particles/model.py
@@ -10,6 +10,7 @@ from invenio_i18n import lazy_gettext as _
 from invenio_records_permissions.generators import AuthenticatedUser
 from oarepo_model.api import model
 from oarepo_model.customizations import (
+    AddFacetGroup,
     AddMetadataExport,
     PrependMixin,
     SetDefaultSearchFields,
@@ -61,6 +62,19 @@ particles_model = model(
             "metadata.number_of_events",
             "metadata.recid",
             "metadata.collision_information",
+        ),
+        AddFacetGroup(
+            name="default",
+            facets=[
+                "metadata.experiment",
+                "metadata.category",
+                "metadata.dataset_type",
+                "metadata.collision_information.type",
+                "metadata.file_types",
+                # TODO: somehow include "metadata.dates.created.year",
+                "metadata.number_of_events",
+
+            ],
         ),
     ],
     configuration={"ui_blueprint_name": "particles_ui"},
--- sample_data/Delphi/sample_upload.py
+++ sample_data/Delphi/sample_upload.py
@@ -194,7 +194,8 @@ def fill_cern_metadata(source) -> dict:
     license_info = src.get("license", {})
     if isinstance(license_info, dict):
         attr = str(license_info.get("attribution", "")).lower()
-        if "cc0-1.0" in attr:
+        if "cc0-1.0" in attr:
+
             tgt["rights"] = [{"id": "CC0-1.0"}]
 
     # Identifiers (append DOI/OAI if present)
@@ -225,21 +226,21 @@ def fill_cern_metadata(source) -> dict:
     elif isinstance(secondary, list) and any(str(s).lower() == "logbook" for s in secondary):
         # TODO: fix values
         tgt["dataset_type"] = "logbook"
-        tgt["resource_type"] = {"id": "publication"}
+        tgt["resource_type"] = {"id": "H41Y-FW7B"}  # logbook
         tgt.pop("collision_information", None)  # logbooks don't have collision info
         tgt.pop("number_of_events", None)  # logbooks don't have experiment field
         metadata["metadata"] = tgt
         return metadata
     elif isinstance(secondary, list) and any(str(s).lower() == "manual" for s in secondary):
         tgt["dataset_type"] = "manual"
-        tgt["resource_type"] = {"id": "publication"}
+        tgt["resource_type"] = {"id": "c_71bd"} # manual
         tgt.pop("collision_information", None)  # logbooks don't have collision info
         tgt.pop("number_of_events", None)  # logbooks don't have experiment field
         metadata["metadata"] = tgt
         return metadata
     elif isinstance(secondary, list) and any(str(s).lower() == "report" for s in secondary):
         tgt["dataset_type"] = "report"
-        tgt["resource_type"] = {"id": "publication"}
+        tgt["resource_type"] = {"id": "c_93fc"} # report
         tgt.pop("collision_information", None)  # logbooks don't have collision info
         tgt.pop("number_of_events", None)  # logbooks don't have experiment field
         metadata["metadata"] = tgt
@@ -290,13 +291,15 @@ async def create_local_client():
     # securely enter the token
     if token == "":
         token = getpass.getpass("Enter API token for repository: ").strip()
-
-    config.add_repository(RepositoryConfig(
-        alias="physica-local",
-        url=URL("https://127.0.0.1:5000/"),
-        token=token,
-        verify_tls=False
-    ))
+    try:
+        config.add_repository(RepositoryConfig(
+            alias="physica-local",
+            url=URL("https://127.0.0.1:5000/"),
+            token=token,
+            verify_tls=False
+        ))
+    except Exception as exc:
+        raise SystemExit(f"Failed to configure repository client: {exc}")
     return await get_async_client("physica-local", config=config)
 
 
